5b937896f17ea2b8abed65b09d527465559781b4
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-15 18:09:30 +0300

    Board setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const board = document.querySelector("#board");
+  const score = document.querySelector(".score span")
 
   return (
     <>
-      
+      <div className='wrapper-score'>
+        <h1 className="score">
+            Score
+            <span>0</span>
+        </h1>
+
+        <h1 className="high-score">
+          High Score
+            <span>0</span>
+        
+        </h1>
+      </div>
+
+      <canvas id="board"></canvas>
+
+      <div className="wrapper-gameover">
+        <h1 className="gameover">Game Over</h1>
+        <button className="restart">Restart</button>
+      </div>
     </>
   )
 }
